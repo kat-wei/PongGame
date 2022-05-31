ebabfa89d57edaf309b9a9b1e042d261afc45c6e
Katherine <106350911+kat-wei@users.noreply.github.com>
2022-05-31 14:20:34 -0600

    Update Renderable.java

--- Renderable.java
+++ Renderable.java
@@ -9,10 +9,7 @@ package pong;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 
-/**
- *
- * @author wkranz
- */
+
 public interface Renderable {
     void draw(Canvas canvas, Color color);
 }
